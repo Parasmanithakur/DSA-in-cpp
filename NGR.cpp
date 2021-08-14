ee357f459aed89f74802cb65d4321955da3eaabe
Parasmani thakur <paras.parasthakur.thakur@gmail.com>
2021-08-14 17:21:53 +0530

    Stack NGR NGL NSL NSR

--- NGR.cpp
+++ NGR.cpp
@@ -3,22 +3,14 @@
 //stack question
 #include <vector>
 #include <stack>
+#include <algorithm>
 #include <iostream>
-
 using namespace std;
-int main()
+vector<int> NGR(vector<int> arr)
 {
-    int n;
-    cin >> n;
-    vector<int> arr;
-    for (int i = 0; i < n; i++)
-    {
-        int x;
-        cin >> x;
-        arr.push_back(x);
-    }
     stack<int> s;
     vector<int> ans;
+    int n = arr.size();
     for (int i = n - 1; i >= 0; i--) // traversing in reverse
     {
 
@@ -36,6 +28,98 @@ int main()
     {
         cout << i << " ";
     }
+    return ans;
+}
+vector<int> NGL(vector<int> arr)
+{
+    stack<int> s;
+    vector<int> ans;
+    int n = arr.size();
+    for (int i = 0; i < n; i++) // traversing in reverse
+    {
+
+        if (!s.empty())
+        {
+            while (!s.empty() && s.top() <= arr[i])
+                s.pop(); // poping out numbers ,As this no. will not be required
+        }
+
+        ans.push_back(s.empty() ? -1 : s.top()); //-1 if no NGR, s.top() if ngr present
+        s.push(arr[i]);
+    }
+    //reverse(ans.begin(), ans.end()); //reversing
+    for (int i : ans)
+    {
+        cout << i << " ";
+    }
+    return ans;
+}
+vector<int> NSL(vector<int> arr)
+{
+    stack<int> s;
+    vector<int> ans;
+    int n = arr.size();
+    for (int i = 0; i < n; i++) // traversing in reverse
+    {
+
+        if (!s.empty())
+        {
+            while (!s.empty() && s.top() >= arr[i])
+                s.pop(); // poping out numbers ,As this no. will not be required
+        }
+
+        ans.push_back(s.empty() ? -1 : s.top()); //-1 if no NGR, s.top() if ngr present
+        s.push(arr[i]);
+    }
+    //reverse(ans.begin(), ans.end()); //reversing
+    for (int i : ans)
+    {
+        cout << i << " ";
+    }
+    return ans;
+}
+vector<int> NSR(vector<int> arr)
+{
+    stack<int> s;
+    vector<int> ans;
+    int n = arr.size();
+    for (int i = n - 1; i >= 0; i--) // traversing in reverse
+    {
+
+        if (!s.empty())
+        {
+            while (!s.empty() && s.top() >= arr[i])
+                s.pop(); // poping out numbers ,As this no. will not be required
+        }
+
+        ans.push_back(s.empty() ? -1 : s.top()); //-1 if no NGR, s.top() if ngr present
+        s.push(arr[i]);
+    }
+    reverse(ans.begin(), ans.end()); //reversing
+    for (int i : ans)
+    {
+        cout << i << " ";
+    }
+    return ans;
+}
+int main()
+{
+    int n;
+    cin >> n;
+    vector<int> arr;
+    for (int i = 0; i < n; i++)
+    {
+        int x;
+        cin >> x;
+        arr.push_back(x);
+    }
+    NGR(arr);
     cout << endl;
+    NGL(arr);
+    cout << endl;
+    NSL(arr);
+    cout << endl;
+    NSR(arr);
+
     return 0;
 }
