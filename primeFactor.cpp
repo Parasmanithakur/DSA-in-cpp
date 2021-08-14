b3ba8bd2f93e7c130df2fcb149f1d8ea329ef20e
Parasmani thakur <paras.parasthakur.thakur@gmail.com>
2021-08-14 16:48:08 +0530

    Sieve Prime factors

---
+++ primeFactor.cpp
@@ -0,0 +1,38 @@
+//prime factors of number
+#include <iostream>
+#include <vector>
+
+using namespace std;
+void primefactor(int n)
+{
+    int spf[200] = {0};
+    for (int i = 2; i <= n; i++)
+    {
+        spf[i] = i;
+    }
+    for (int i = 2; i <= n; i++) //usinf seive
+    {
+        if (spf[i] == i)
+        {
+            for (int j = i * i; j <= n; j += i)
+            {
+                if (spf[j] == j)
+                {
+                    spf[j] = i;
+                }
+            }
+        }
+    }
+    while (n != 1)
+    {
+        cout << spf[n] << " ";
+        n = n / spf[n];
+    }
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+    primefactor(n);
+}
